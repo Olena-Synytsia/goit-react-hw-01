df7f851e6f28cc3449d0477b98e9ebd9d72316af
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-08-31 11:34:35 +0300

    profile start

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,24 @@ import FriendList from "./components/FriendList/FriendList";
 import FriendListItem from "./components/FriendListItem/FriendListItem";
 import Profile from "./components/Profile/Profile";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
+import userData from "./components/Profile/UserData.json";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div>
       <FriendList />
       <FriendListItem />
-      <Profile />
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
       <TransactionHistory />
-    </>
+    </div>
   );
-  // const [count, setCount] = useState(0);
-  // return (
-  //   <>
-  //     <div>
-  //       <a href="https://vitejs.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.jsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // );
 }
 
 export default App;
