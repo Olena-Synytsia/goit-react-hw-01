404fad27cd54fd732b70936685651b9278865bb1
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-09-01 10:06:22 +0300

    HW finish

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,21 +2,21 @@ import s from "./TransactionHistory.module.css";
 
 const TransactionHistory = ({ items }) => {
   return (
-    <table>
-      <thead>
+    <table className={s.table}>
+      <thead className={s.thead}>
         <tr>
           <th className={s.title}>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={s.title}>Amount</th>
+          <th className={s.title}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {items.map(({ id, type, amount, currency }) => (
           <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+            <td className={s.cell}>{type}</td>
+            <td className={s.cell}>{amount}</td>
+            <td className={s.cell}>{currency}</td>
           </tr>
         ))}
       </tbody>
